4982ee4ad80e6af7e175304f5e2c9d92ebe33bfe
racine14creator <bisimwagrace596@gmail.com>
2025-01-09 02:28:34 +0200

    The UI update

--- config/index.js
+++ config/index.js
@@ -1,16 +1,15 @@
-const mysql = require('mysql')
-const { success, error } = require('consola')
+const mysql = require("mysql");
+const { success, error } = require("consola");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'node'
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node",
+});
 db.connect((err) => {
-    if(err) 
-        error({ message: `Error: ${err}`, badge: true })
-    success({ message: `Vous etez connecte a la base des donnees`, badge: true })
-})
+  if (err) error({ message: `Error: ${err}`, badge: true });
+  success({ message: `Vous etez connecte a la base des donnees`, badge: true });
+});
 
-module.exports = db
+module.exports = db;
